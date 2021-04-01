df0ee5ad30ce1aeb1506746f90fbc5780447054c
Martin Tsvetkov <sc19mmt@leeds.ac.uk>
2021-04-01 15:42:07 +0300

    removed build artifacts

--- build/libs/raylib/src/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/marti/Documents/StringBeat/libs/raylib/src
-# Build directory: C:/Users/marti/Documents/StringBeat/build/libs/raylib/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(pkg-config--static "C:/Users/marti/Documents/StringBeat/libs/raylib/src/../cmake/test-pkgconfig.sh" "--static")
-set_tests_properties(pkg-config--static PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/marti/Documents/StringBeat/libs/raylib/src/CMakeLists.txt;180;add_test;C:/Users/marti/Documents/StringBeat/libs/raylib/src/CMakeLists.txt;0;")
-subdirs("external/glfw")
